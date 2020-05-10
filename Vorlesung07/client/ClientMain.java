76cdca7c064a0441b088a55b8c346b08bfb8b735
kigari94 <xxchistylexx@googlemail.com>
2020-05-10 12:40:47 +0200

    adding username handling

--- Vorlesung07/client/ClientMain.java
+++ Vorlesung07/client/ClientMain.java
@@ -1,4 +1,5 @@
 package client;
+
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -6,58 +7,57 @@ import java.util.Scanner;
 public class ClientMain {
 
 	private boolean isRunning = true;
-	
+
 	public void createClient() {
-		
+
 		try {
 			Socket socket = new Socket("127.0.0.1", 3445);
 			PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
 			Scanner scanner = new Scanner(socket.getInputStream());
+
 			System.out.println("Bitte gib einen username ein: ");
 			Scanner name = new Scanner(System.in);
 			String username = name.next();
 			System.out.println("Bitte gib eine Nachricht ein:");
-			
+
 			// Alt + shift + m
-			writeMessage(printWriter);
-			
-			while(isRunning) {
+			writeMessage(printWriter, username);
+
+			while (isRunning) {
 				System.out.println(scanner.nextLine());
 			}
-			
+
 			scanner.close();
 			printWriter.close();
 			socket.close();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-
-	private void writeMessage(PrintWriter printWriter) {
+	private void writeMessage(PrintWriter printWriter, String username) {
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				Scanner keyboard = new Scanner(System.in);
-				while(isRunning) {
-					String text = keyboard.nextLine();
-					if(text.equalsIgnoreCase("Quit")) {
+				while (isRunning) {
+					String text = username + ": " + keyboard.nextLine();
+					if (text.equalsIgnoreCase("Quit")) {
 						isRunning = false;
 						System.out.println("Verbindung getrennt");
 					} else {
 						printWriter.println(text);
-						printWriter.flush();							
+						printWriter.flush();
 					}
 				}
 				keyboard.close();
 			}
 		}).start();
 	}
-	
-	
+
 	public static void main(String[] args) {
 		ClientMain client = new ClientMain();
 		client.createClient();
