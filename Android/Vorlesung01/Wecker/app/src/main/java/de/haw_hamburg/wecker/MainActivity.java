a51f8305b3a315a41d4a9342be728a2d59308e91
kigari94 <36a8d9lf21x>
2020-05-25 16:59:28 +0200

    First set of app layouts

---
+++ Android/Vorlesung01/Wecker/app/src/main/java/de/haw_hamburg/wecker/MainActivity.java
@@ -0,0 +1,30 @@
+package de.haw_hamburg.wecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButtonMain);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Want to add a new entry",
+                        Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, EditAlarmActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
